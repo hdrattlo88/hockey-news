e79560b37190421b41e1fd3b01d4b28e12f20878
Heather Drattlo <hdrattlo88@yahoo.com>
2018-12-11 20:29:25 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,130 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose")
+// Require all models
+var db = require("./models");
+
+var PORT = 8888;
+
+// Initialize Express
+var app = express();
+
+// First, tell the console what server2.js is doing
+console.log("\n******************************************\n" +
+            "Grabbing every article headline and link\n" +
+            "from the NHL website:" +
+            "\n******************************************\n");
+
+
+
+
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/scrapingdb", {useNewUrlParser: true});
+//routes
+            // A GET route for scraping the nhl website
+app.get("/scrape", function(req,res){
+// Making a request via axios for `nhl.com`'s homepage
+axios.get("https://www.nhl.com/").then(function(response) {
+
+  // Load the body of the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Empty array to save our scraped data
+  
+
+  // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
+  $("h4.headline-link").each(function(i, element) {
+var result = [];
+    // Save the text of the h4-tag as "title"
+    var title = $(element).text();
+
+    // Find the h4 tag's parent a-tag, and save it's href value as "link"
+    var link = $(element).parent().attr("href");
+
+    // Make an object with data we scraped for this h4 and push it to the results array
+    result.push({
+      title: title,
+      link: link
+    });
+
+    db.Article.create(result)
+    .then(function(dbArticle) {
+      // View the added result in the console
+      console.log(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, log it
+      console.log(err);
+    });
+
+  });
+
+  // After looping through each h4.headline-link, log the results
+  console.log(results);
+});
+});
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
